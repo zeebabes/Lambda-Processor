d0f6160de97f2cef87e396c3bffe94955c55d6eb
KemiA <kzagbabiaka@gmail.com>
2025-05-30 02:13:06 -0400

    update

--- lambda_function.py
+++ lambda_function.py
@@ -2,4 +2,16 @@ import json
 
 def lambda_handler(event, context):
     print("Event received:", json.dumps(event))
-    return {"statusCode": 200, "body": "File processed."}
+
+    # Check if it's an S3 event
+    if "Records" in event and event["Records"][0].get("eventSource") == "aws:s3":
+        return {
+            "statusCode": 200,
+            "body": "S3 upload processed."
+        }
+
+    # Otherwise, assume API Gateway
+    return {
+        "statusCode": 200,
+        "body": json.dumps("API Gateway call successful!")
+    }
